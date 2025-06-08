2d5179a80ea7c21c6f73241edb5cee9ecd303093
Elia Brian Baskoro <eliabrianb@gmail.com>
2025-06-08 19:24:57 +0700

    add logo and favicon

---
+++ resources/views/filament/components/brand.blade.php
@@ -0,0 +1,4 @@
+<img src="{{ asset('logo.png') }}" alt="Logo" class="h-10" style="align-self:center">
+<div class="px-3" style="align-self:center">
+    <h1 class="font-bold text-xl leading-5 tracking-tight dark:text-white">Natural Twenty</h1>
+</div>
